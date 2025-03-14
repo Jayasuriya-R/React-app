345c3f55bee28911bd0c02c477129dc4374a5125
Jayasuriya-R <suryaabi3801@gmail.com>
2025-03-14 10:30:59 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1",{
+    id:"title"
+    },"Hi everyone");
+
+    const heading1 = React.createElement("h2",{
+    id:"title" 
+    },"Hello everyone");
+
+    const container = React.createElement("div",{
+    id:"container"
+    },[heading,heading1]);
+
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+
+    
+
+    root.render(container);
